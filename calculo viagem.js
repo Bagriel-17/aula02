95973c4b720f6e8dae29f5b71a2417c2c9041819
Bagriel-17 <RM95012@estudante.fieb.edu.br>
2025-11-14 17:25:54 -0300

    finalização calculo de viagem

--- calculo viagem.js
+++ calculo viagem.js
@@ -1,6 +1,12 @@
+
 let tempo = 12
+
 let velocidade_media = 60
+
 let litros_abastecidos = 44
+
+let combustivel = 5 // se for 0= é igual a etanol. se for 1= gasolina .se for 2= diesel 
+
 let preco_etanol = 3.14
 let preco_diesel = 6.10
 let preco_gasolina = 6.17
@@ -11,6 +17,12 @@ let preco_viagem_diesel = (consumo_medio * preco_diesel)
 let preco_viagem_alcool = (consumo_medio * preco_etanol) 
 console.log(`a distancia da viagem é ${distancia}KM`)
 console.log(`o consumo da viagem em litros é de ${(consumo_medio).toLocaleString('pt-br',)} litros`)
-console.log(`o que você gastou com gasolina foi ${(preco_viagem_gasolina).toLocaleString('pt-br',{style: 'currency', currency: 'brl'})}` )
-console.log(`o que você gastou com diesel foi ${(preco_viagem_diesel).toLocaleString("pt-br",{style: 'currency', currency: 'brl'})}` )
+if (combustivel == 0){
 console.log(`o que você gastou com alcool foi ${(preco_viagem_alcool).toLocaleString("pt-br",{style: 'currency', currency: 'brl'})}` )
+}else if (combustivel == 1){
+console.log(`o que você gastou com gasolina foi ${(preco_viagem_gasolina).toLocaleString('pt-br',{style: 'currency', currency: 'brl'})}` )
+}else if (combustivel == 2){
+console.log(`o que você gastou com diesel foi ${(preco_viagem_diesel).toLocaleString("pt-br",{style: 'currency', currency: 'brl'})}` )
+}else{
+    console.log('tipo de combustivel não encontrado')
+}
