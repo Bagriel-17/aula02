bd0e1cdad339d7ef36a22043ababc666c7a6e25d
Bagriel-17 <RM95012@estudante.fieb.edu.br>
2025-11-14 15:11:17 -0300

    calculo viagem continuação 1

--- calculo viagem.js
+++ calculo viagem.js
@@ -1,3 +1,7 @@
-let t = "5"
-let Vlm = "90"
-let cons = "13"
+let tempo = 18
+let velocidade_media = 100
+let litros_abastecidos = 97
+let distancia = 3000
+let consumo_medio = (distancia/litros_abastecidos)
+let distancia_total = (velocidade_media * tempo)
+
