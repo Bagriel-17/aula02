126aba38071df809af9e2a30f6002ed79bbfb03f
Bagriel-17 <RM95012@estudante.fieb.edu.br>
2025-11-14 14:30:14 -0300

    exercicio imc finalizado

--- imc.js
+++ imc.js
@@ -8,7 +8,7 @@ if (imc <= 18.5){
     console.log(nome + ` você tem execesso de peso seu imc é ` + imc)
 } else if ( imc >= 30 && imc <= 34.9){
     console.log(nome + ` você obesidade classe 1 seu imc é ` + imc)
-} else if ( imc >= 35 && imc <= 34.9){
+} else if ( imc >= 35 && imc <= 39.9){
     console.log(nome + ` você obesidade classe 2 seu imc é ` + imc)
 }else {
     console.log(nome + ` você obesidade classe 3 seu imc é ` + imc)
