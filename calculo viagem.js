f337aa17d5e6a6ca369a27b118d7a61f4d6e1f56
Bagriel-17 <RM95012@estudante.fieb.edu.br>
2025-11-14 14:57:27 -0300

    correção imc

---
+++ calculo viagem.js
@@ -0,0 +1,3 @@
+let t = "5"
+let Vlm = "90"
+let cons = "13"
--- imc.js
+++ imc.js
@@ -1,14 +1,14 @@
-let nome = 'yago', peso = 78, altura = 1.76;
+let nome = 'yago', peso = 80, altura = 1.76;
 imc = peso / (altura **2) 
 if (imc <= 18.5){
     console.log(nome + ` seu peso esta a baixo do ideal seu imc é ` + imc)
-}else if ( imc >= 18.6 && imc <= 24.9){
+}else if ( imc >= 18.6 && imc < 24.9){
     console.log(nome + ` seu peso normal seu imc é ` + imc)
-} else if ( imc >= 25 && imc <= 29.9){
+} else if ( imc >= 24.9 && imc < 29.9){
     console.log(nome + ` você tem execesso de peso seu imc é ` + imc)
-} else if ( imc >= 30 && imc <= 34.9){
+} else if ( imc >= 29.9 && imc < 34.9){
     console.log(nome + ` você obesidade classe 1 seu imc é ` + imc)
-} else if ( imc >= 35 && imc <= 39.9){
+} else if ( imc >= 34.9 && imc < 39.9){
     console.log(nome + ` você obesidade classe 2 seu imc é ` + imc)
 }else {
     console.log(nome + ` você obesidade classe 3 seu imc é ` + imc)
